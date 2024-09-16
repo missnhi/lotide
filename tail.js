3e6eff09b3311c9d15172ffce6a33f4dfc2064cd
missnhi <nhi.phan.ley@gmail.com>
2024-09-16 16:54:44 -0600

    tail property

---
+++ tail.js
@@ -0,0 +1,24 @@
+const assertEqual = function (actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`)
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`)
+  }
+};
+
+// FUNCTION IMPLEMENTATION
+// Create a function tail which returns the "tail" of an array: everything except for the first item (head) of the provided array.
+const tail = function (array) {
+  return array.slice(1);
+};
+
+// TEST CODE
+const result = tail(["Hello", "Lighthouse", "Labs"]);
+assertEqual(result.length, 2); // ensure we get back two elements
+assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
+assertEqual(result[1], "Labs"); // ensure second element is "Labs"
+
+// Test Case: Check the original array
+const words = ["Yo Yo", "Lighthouse", "Labs"];
+tail(words); // no need to capture the return value since we are not checking it
+assertEqual(words.length, 3); // original array should still have 3 elements!
