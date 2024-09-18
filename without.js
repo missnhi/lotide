fc66ce4fe3d501b4086c4a2ad6609172dfc88fbf
missnhi <nhi.phan.ley@gmail.com>
2024-09-17 23:41:56 -0600

    Implement without which will return a subset of a given array, removing unwanted elements.

---
+++ without.js
@@ -0,0 +1,44 @@
+
+// Implement without which will return a subset of a given array, removing unwanted elements.
+
+const assertArraysEqual = function (array1, array2) {
+  if (eqArrays(array1, array2)) {
+    console.log(`✅✅✅ Assertion Passed: ${array1} === ${array2}`)
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${array1} !== ${array2}`)
+  }
+}
+
+const eqArrays = function (array1, array2) {
+  // check if the arrays are the same length
+  if (array1.length !== array2.length) {
+    return false;
+  }
+  // check if the arrays are the same
+  for (let i in array1) {
+    if (array1[i] !== array2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+
+// IMPLEMENTATION
+const without = function (source, itemsToRemove) {
+  let newArray = [];
+  for (let i in source) {
+    if (!itemsToRemove.includes(source[i])) {
+      newArray.push(source[i]);
+    }
+  }
+  return newArray;
+};
+
+// test code
+assertArraysEqual(without([1, 2, 3], [1]), [2, 3]); // => should PASS
+assertArraysEqual(without(["1", "2", "3"], [1, 2, "3"]), ["1", "2"]); // => should PASS
+// Write a test case to make sure that the original array is not modified.
+const words = ["hello", "world", "lighthouse"];
+without(words, ["lighthouse"]); // no need to capture return value for this test case
+assertArraysEqual(words, ["hello", "world", "lighthouse"]); // => should PASS
