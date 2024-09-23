3836706e9e78fe4762cd51bd211ff3c3eca81625
missnhi <nhi.phan.ley@gmail.com>
2024-09-23 16:37:57 -0600

    require util inspect

---
+++ assertObjectsEqual.js
@@ -0,0 +1,49 @@
+const eqArrays = function(array1, array2) {
+  // check if the arrays are the same length
+  if (array1.length !== array2.length) {
+    return false;
+  }
+  // check if the arrays are the same
+  for (let i in array1) {
+    if (array1[i] !== array2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+
+const eqObjects = function(object1, object2) {
+  // 1. same number of keys
+  if (Object.keys(object1).length !== Object.keys(object2).length) {
+    return false;
+  }
+  // 2. The value for each key in one object is the same
+  // as the value for that same key in the other object
+  for (const key in object1) {
+    if (Array.isArray(object1[key]) && Array.isArray(object2[key])) {
+      if (!eqArrays(object1[key], object2[key])) {
+        return false;
+      }
+    } else if (object1[key] !== object2[key]) {
+      return false;
+    }
+  }
+  return true;
+}
+
+// FUNCTION IMPLEMENTATION
+const assertObjectsEqual = function(actual, expected) {
+  const inspect = require("util").inspect;
+  if (eqObjects(actual, expected)) {
+    console.log(`✅✅✅ Assertion Passed: ${inspect(actual)} === ${inspect(expected)}`)
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${inspect(actual)} !== ${inspect(expected)}`)
+  }
+};
+
+// TEST CODE
+const actual = {colors: ["red", "blue"], size: "medium"};
+const expected = {size: "medium", colors: ["red", "blue"]};
+assertObjectsEqual(actual, expected);
+
