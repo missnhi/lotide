2dd93cc9a278c5010b2057277a00224ba125d128
missnhi <nhi.phan.ley@gmail.com>
2024-10-01 10:47:47 -0600

    findKey in lotide

---
+++ findKey.js
@@ -0,0 +1,33 @@
+//FUNCTION HELPER
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`)
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`)
+  }
+};
+//ACTUAL FUNCTION
+const findKey = function(object, callback) {
+  for (let key in object) {
+    if (callback(object[key])) {
+      return key;
+    }
+  }
+}
+
+//TEST CODE
+
+assertEqual(
+  findKey(
+    {
+      "Blue Hill": {stars: 1},
+      Akaleri: {stars: 3},
+      noma: {stars: 2},
+      elBulli: {stars: 3},
+      Ora: {stars: 2},
+      Akelarre: {stars: 3},
+    },
+    (x) => x.stars === 2
+  ),
+  "noma"
+); // => should PASS
