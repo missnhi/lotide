ea392a62ca380c0e0c227367629a20c5598db4f0
missnhi <nhi.phan.ley@gmail.com>
2024-10-02 22:11:20 -0600

    refactor eqArrays

--- eqArrays.js
+++ eqArrays.js
@@ -1,15 +1,5 @@
 // a function that can compare two arrays for a perfect match
-
-const assertEqual = function (actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`)
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`)
-  }
-};
-
-// function implementation
-const eqArrays = function (array1, array2) {
+const eqArrays = function(array1, array2) {
   // check if the arrays are the same length
   if (array1.length !== array2.length) {
     return false;
@@ -22,9 +12,4 @@ const eqArrays = function (array1, array2) {
   }
   return true;
 };
-
-console.log(eqArrays([1, 2, 3], [1, 2, 3])); // => true
-console.log(eqArrays([1, 2, 3], [3, 2, 1])); // => false
-
-console.log(eqArrays(["1", "2", "3"], ["1", "2", "3"])); // => true
-console.log(eqArrays(["1", "2", "3"], ["1", "2", 3])); // => false
+module.exports = eqArrays;
