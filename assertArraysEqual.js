7a1cc92362fb62ed440f655144b589ea2dd82c63
missnhi <nhi.phan.ley@gmail.com>
2024-10-02 22:14:24 -0600

    refactor assertArraysEqual.js

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -14,8 +14,3 @@ const assertArraysEqual = function(array1, array2) {
 
 module.exports = assertArraysEqual;
 
-// TEST CODE
-assertArraysEqual([1, 2, 3], [1, 2, 3]); // => true
-assertArraysEqual([1, 2, 3], [3, 2, 1]); // => false
-assertArraysEqual(["1", "2", "3"], ["1", "2", "3"]); // => true
-assertArraysEqual(["1", "2", "3"], ["1", "2", 3]); // => false
